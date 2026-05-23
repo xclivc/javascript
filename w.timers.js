373a3885bef738378fffe2c9cbf1a3e7df9b430a
xclivc <63088230+xclivc@users.noreply.github.com>
2026-05-23 17:09:52 +0800

    Update w.timers.js

--- w.timers.js
+++ w.timers.js
@@ -1,1 +1,29 @@
-w.timers.js
+function workerTimers() {
+    var idCounter = Date.now();
+    const script = `self.onmessage = (function (self) { const handles = [setTimeout, clearTimeout, setInterval, clearInterval].reduce((result, fn) => (result[fn.name] = fn, result), {}); const ids = []; return function (event) { const { id = 0, type = '', delay = 0 } = event.data || {}; const handle = handles[type]; if (!handle) return self.postMessage({ id }); if (handle.name.startsWith('set')) { ids[id] = handle(() => self.postMessage({ id, type }), delay || 0); } else { handle(ids[id]); } }; })(self);`;
+    const blob = new Blob([script], { type: 'application/javascript' });
+    const worker = new Worker(URL.createObjectURL(blob));
+    const callbacks = {};
+    worker.onmessage = (e) => {
+        const { id, type } = e.data || {};
+        if (!id || !type) return;
+        const callback = callbacks[id];
+        if (!callback) return;
+        callback();
+    };
+    return function (type, ...args) {
+        if (!type) return worker.terminate();
+        if (args.length == 2 && typeof (args[0]) == 'function') {
+            const id = ++idCounter;
+            const callback = args[0];
+            const delay = args[1];
+            callbacks[id] = callback;
+            worker.postMessage({ id, type, delay });
+            return id;
+        } else if (args.length == 1 && typeof (args[0]) == 'number') {
+            const id = args[0];
+            worker.postMessage({ id, type });
+            return;
+        }
+    };
+}
